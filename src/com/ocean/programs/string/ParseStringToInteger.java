e7492bda3e26ceff885a7b90846a243c1e5b8581
Kanhaiya Thakur <kanhaiya.thakur@in.g4s.com>
2018-05-04 11:32:22 +0000

    Adding changes

--- src/com/ocean/programs/string/ParseStringToInteger.java
+++ src/com/ocean/programs/string/ParseStringToInteger.java
@@ -1,5 +1,9 @@
 package com.ocean.programs.string;
 
+import java.util.Arrays;
+
+// Ascii code of 0 = 48, 1 = 49, 2=50 .. 9 = 57
+
 public class ParseStringToInteger 
 {
 	public static void main(String[] args) 
@@ -7,8 +11,16 @@ public class ParseStringToInteger
 		String source = "123";
 		
 		char[] charArray = source.toCharArray();
+		int[] asciiArray = new int[charArray.length];
 		
 		for(int i=0;i<charArray.length;i++)
+		{
+			asciiArray[i] = (int) charArray[i];
+		}
+		
+		System.out.println(Arrays.toString(asciiArray));
+		
+		for(int i=0;i<asciiArray.length;i++)
 		{
 			
 		}
