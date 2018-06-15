5ae0ea42e10d848083610e43f28384c3357023ab
Kanhaiya Thakur <kanthaku1@publicisgroupe.net>
2018-06-15 12:36:22 +0530

    Adding changes

--- src/com/ocean/programs/tricky/ListAndLinkedList.java
+++ src/com/ocean/programs/tricky/ListAndLinkedList.java
@@ -8,8 +8,16 @@ public class ListAndLinkedList
 	public static void main(String[] args) 
 	{
 		LinkedList<String> linkedList = new LinkedList<String>();
+		linkedList.addFirst("Two");
+		linkedList.addLast("Three");
+		linkedList.addFirst("One");
+		linkedList.addLast("Four");
+		System.out.println(linkedList);
 		linkedList.removeFirst();
+		linkedList.removeLast();
+		System.out.println(linkedList);
 		
+		// LinkedList extends deque, that's why removeFirst() is there 
 		List<String> list = new LinkedList<String>();
 		//list.removeFirst();	// removeFirst() doesn't exist	
 		// Here list can access all methods of class List and all overridden methods of class List by LinkedList
