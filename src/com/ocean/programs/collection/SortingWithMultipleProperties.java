6b9887a51ac26e6dd1e0fbd3289cd175c23c452c
Kanhaiya Thakur <kanthaku1@publicisgroupe.net>
2018-06-18 13:54:14 +0530

    Adding changes

--- src/com/ocean/programs/collection/SortingWithMultipleProperties.java
+++ src/com/ocean/programs/collection/SortingWithMultipleProperties.java
@@ -1,6 +1,8 @@
 package com.ocean.programs.collection;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortingWithMultipleProperties 
@@ -8,24 +10,80 @@ public class SortingWithMultipleProperties
 	public static void main(String[] args) 
 	{
 		List<Student> list = new ArrayList<>();
-		list.add(new Student(15, "Raju", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(14, "Ashutosh", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(13, "Sandeep", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(12, "Rajeev", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(11, "Kanhaiya", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(10, "Aarti", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(9, "Chandra", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(8, "Varsha", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(7, "Varsha2", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(6, "Sandeep2", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(5, "Ashutosh2", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(4, "Rajeev2", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(3, "Aarti2", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(2, "Rajeev3", "mid_a", "15_last", "15_roll"));
-		list.add(new Student(19, "Rajeev5", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(17, "Rajeev7", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(16, "Rajeev4", "15_middle", "15_last", "15_roll"));
-		list.add(new Student(18, "Rajeev6", "15_middle", "15_last", "15_roll"));
+		list.add(new Student(2, "Ocean3", "", "", ""));
+		list.add(new Student(2, "Ocean3", "", "", ""));
+		list.add(new Student(2, "Ocean3", "", "", ""));
+		list.add(new Student(2, "Ocean3", "", "", ""));
+		list.add(new Student(2, "Ocean3", "", "", ""));
+		list.add(new Student(2, "Ocean3", "", "", ""));
+		list.add(new Student(2, "Ocean3", "", "", ""));
+		list.add(new Student(2, "Ocean3", "", "", ""));
+		list.add(new Student(2, "Ocean2", "", "", ""));
+		list.add(new Student(2, "Ocean2", "", "", ""));
+		list.add(new Student(2, "Ocean2", "", "", ""));
+		list.add(new Student(2, "Ocean2", "", "", ""));
+		list.add(new Student(2, "Ocean2", "", "", ""));
+		list.add(new Student(2, "Ocean2", "", "", ""));
+		list.add(new Student(2, "Ocean2", "", "", ""));
+		list.add(new Student(2, "Ocean2", "", "", ""));
+		list.add(new Student(2, "Ocean1", "mid3", "Life8", "rollNo7"));
+		list.add(new Student(2, "Ocean1", "mid3", "Life8", "rollNo6"));
+		list.add(new Student(2, "Ocean1", "mid3", "Life7", "rollNo5"));
+		list.add(new Student(2, "Ocean1", "mid3", "Life7", "rollNo5"));
+		list.add(new Student(2, "Ocean1", "mid3", "Life7", "rollNo5"));
+		list.add(new Student(2, "Ocean1", "mid3", "Life7", "rollNo4"));
+		list.add(new Student(2, "Ocean1", "mid3", "Life7", "rollNo3"));
+		list.add(new Student(2, "Ocean1", "mid3", "Life7", "rollNo5"));
+		list.add(new Student(2, "Ocean1", "mid2", "Life5", "rollNo2"));
+		list.add(new Student(2, "Ocean1", "mid2", "Life5", "rollNo2"));
+		list.add(new Student(2, "Ocean1", "", "Life8", "rollNo9"));
+		list.add(new Student(2, "Ocean1", "", "Life8", "rollNo8"));
+		list.add(new Student(2, "Ocean1", "", "Life8", "rollNo7"));
+		list.add(new Student(1, "Ocean5", "", "", ""));
+		list.add(new Student(1, "Ocean5", "", "", ""));
+		list.add(new Student(1, "Ocean5", "", "", ""));
+		list.add(new Student(1, "Ocean5", "", "", ""));
+		list.add(new Student(1, "Ocean5", "", "", ""));
+		list.add(new Student(1, "Ocean4", "", "", ""));
+		list.add(new Student(1, "Ocean4", "", "", ""));
+		list.add(new Student(1, "Ocean4", "", "", ""));
+		
+		Collections.sort(list, new Comparator<Student>() 
+		{
+			@Override
+			public int compare(Student student1, Student student2) 
+			{
+				int c;
+				c = Integer.valueOf(student1.id).compareTo(Integer.valueOf(student2.id));
+				
+				if(c == 0)
+				{
+					c = student1.firstName.compareTo(student2.firstName);
+				}
+				
+				if(c == 0)
+				{
+					c = student1.middleName.compareTo(student2.middleName);
+				}
+				
+				if(c == 0)
+				{
+					c = student1.lastName.compareTo(student2.lastName);
+				}
+				
+				if(c == 0)
+				{
+					c = student1.rollNo.compareTo(student2.rollNo);
+				}
+				
+				return c;
+			}
+		});
+		
+		for(Student student : list)
+		{
+			System.out.println(student);
+		}
 	}
 }
 
@@ -37,12 +95,18 @@ class Student
 	String lastName;
 	String rollNo;
 	
-	public Student(int id, String firstName, String middleName, String lastName, String rollNo) {
-		super();
+	public Student(int id, String firstName, String middleName, String lastName, String rollNo) 
+	{
 		this.id = id;
 		this.firstName = firstName;
 		this.middleName = middleName;
 		this.lastName = lastName;
 		this.rollNo = rollNo;
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "Student [id=" + id + ", firstName=" + firstName + ", middleName=" + middleName + ", lastName=" + lastName + ", rollNo=" + rollNo + "]";
 	}
 }
