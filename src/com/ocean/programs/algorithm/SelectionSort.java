b636d3d14dcd0885e9343045c58f2e2428d230ba
Kanhaiya Thakur <kanthaku1@publicisgroupe.net>
2018-12-14 13:55:34 +0530

    Adding changes

--- src/com/ocean/programs/algorithm/SelectionSort.java
+++ src/com/ocean/programs/algorithm/SelectionSort.java
@@ -2,32 +2,17 @@ package com.ocean.programs.algorithm;
 
 import java.util.Arrays;
 
-/*
-The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning.
+/**
+Selection sort is a simple sorting algorithm. 
+This sorting algorithm is an in-place comparison-based algorithm in which the list is divided into two parts, 
+the sorted part at the left end and the unsorted part at the right end. Initially, 
+the sorted part is empty and the unsorted part is the entire list.
 
-arr[] = 64 25 12 22 11
+The smallest element is selected from the unsorted array and swapped with the leftmost element, 
+and that element becomes a part of the sorted array. This process continues moving unsorted array boundary by one element to the right.
 
-// Find the minimum element in arr[0...4]
-// and place it at beginning
-11 25 12 22 64
-
-// Find the minimum element in arr[1...4]
-// and place it at beginning of arr[1...4]
-11 12 25 22 64
-
-// Find the minimum element in arr[2...4]
-// and place it at beginning of arr[2...4]
-11 12 22 25 64
-
-// Find the minimum element in arr[3...4]
-// and place it at beginning of arr[3...4]
-11 12 22 25 64 
-
-
-Time Complexity: O(n2) as there are two nested loops.
-
-It's called SelectionSort, because we select the minimum valued element by comparing and swaps it.
-*/
+This algorithm is not suitable for large data sets as its average and worst case complexities are of O(n2), where n is the number of items.
+**/
 public class SelectionSort 
 {
 	public static void main(String[] args) 
