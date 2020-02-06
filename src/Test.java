9c47a39b0d873dc09a287d947209b972c46ca489
kanthaku1 <kanthaku1@WKWIN8679663.global.publicisgroupe.net>
2020-02-06 19:12:22 +0530

    adding changes

--- src/Test.java
+++ src/Test.java
@@ -1,32 +1,72 @@
-public class Test 
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) 
+    {
+        int maxLength = 0;
+        int length = 1;
+
+        for(int i=0;i<a.size();i++)
+        {
+            length = 1;
+            for(int j=i+1;j<a.size();j++)
+            {
+            	if(a.get(i) < a.get(j))
+            	{
+            		if(Math.abs(a.get(i)-a.get(j)) == 1)
+            			length++;
+            	}
+            	else if(a.get(i) > a.get(j))
+            	{
+            		if((Math.abs(a.get(i)-a.get(j)) == 1) && (Math.abs((a.get(i)+1)-a.get(j)) == 1))
+            			length++;
+            	}
+            	else if(a.get(i) == a.get(j))
+            	{
+            		length++;
+            	}
+            	
+            	System.out.println(a.get(i)+ " | "+a.get(j));
+            }
+
+            System.out.println("Length: "+length);
+            if(length > maxLength)
+                maxLength = length;
+        }
+
+        return maxLength;
+    }
+
+}
+
+class Test 
 {
-	public static void main(String[] args) {
-		String s = "5</dealer_number>\r\n" + 
-				"    <batch_number>  </batch_number>\r\n" + 
-				"    <sequence_number>   </sequence_number>\r\n" + 
-				"    <card_code></card_code>\r\n" + 
-				"    <adjust_amount>50.00</adjust_amount>\r\n" + 
-				"    <reason_code>34</reason_code>\r\n" + 
-				"    <trans_datetime>2019-02-28T00:00:00</trans_datetime>\r\n" + 
-				"    <adjustment_descript>Prepaid Funds to Marathon </adjustment_descript>\r\n" + 
-				"    <pay_to_dealer_number>92775</pay_to_dealer_number>\r\n" + 
-				"  </cc_pay_adjust>\r\n" + 
-				"  <cnpay_indicator>\r\n" + 
-				"  </dealer_pay_nonpay>\r\n" + 
-				"  <cc_pay_notification>\r\n" + 
-				"    <cc_pay_notice_number>90200590901</cc_pay_notice_number>\r\n" + 
-				"    <pay_to_dealer_number>92775</pay_to_dealer_number>\r\n" + 
-				"    <customer_number>490653</customer_number>\r\n" + 
-				"    <pay_type>3</pay_type>\r\n" + 
-				"    <notification_date>2019-02-28</notification_date>\r\n" + 
-				"    <pay_date>2019-03-01</pay_date>\r\n" + 
-				"    <process_date>2019-02-28</process_date>\r\n" + 
-				"    <deposit_date>2019-02-28</deposit_date>\r\n" + 
-				"  </cc_pay_notification>\r\n" + 
-				"  <cc_pay_adjust>\r\n" + 
-				"    <cc_pay_notice_number>90200590901</cc_pay_notice_number>\r\n" + 
-				"    <line_number>1</line_number>\r\n" + 
-				"    <dealer_number>9277";
-		System.out.println(s.getBytes().length);
-	}
+    public static void main(String[] args) throws IOException 
+    {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        System.out.println(Result.pickingNumbers(a));
+
+        bufferedReader.close();
+    }
 }
